d025ebe369a7ff0a27de5e23085acccf8bbdb38b
douglas <douglasmaxton@gmail.com>
2023-03-08 15:06:22 +0000

    Logic based on captions sends back difference in km

--- src/controllers/botPhotos.js
+++ src/controllers/botPhotos.js
@@ -6,12 +6,68 @@ export const botPhotos = (bot) =>
   bot.on("photo", async (msg) => {
     const chatId = msg.chat.id;
     const fileId = msg.photo[0].file_id;
+
     let firstName = msg.chat.first_name;
     let lastName = msg.chat.last_name;
     let userName = msg.chat.username;
+    let caption = msg.caption;
     let image = await photoToBase64(fileId);
 
-    await postToDB(Photo, { firstName, lastName, userName, image });
+    let captionArray = [];
 
-    await bot.sendMessage(chatId, "Photo Received");
+    if (caption) {
+      if (parseInt(caption)) {
+        Photo.find({ userName }).then(async (response) => {
+          if (response.length === 0) {
+            await postToDB(Photo, {
+              firstName,
+              lastName,
+              userName,
+              caption,
+              image,
+            });
+          } else {
+            //   maps captions stored in db to an array
+            Object.values(response).map((items) => {
+              captionArray.push(items.caption);
+            });
+
+            // if caption is higher that the one sent on Telegram
+            if (captionArray[captionArray.length - 1] < caption) {
+              // save new data to db
+              await postToDB(Photo, {
+                firstName,
+                lastName,
+                userName,
+                caption,
+                image,
+              });
+              // send message to Telegram
+              // sending the difference in km since last photo
+              await bot.sendMessage(
+                chatId,
+                `You have done ${
+                  caption - captionArray[captionArray.length - 1]
+                } km since last reading`
+              );
+            } else {
+              await bot.sendMessage(
+                chatId,
+                "The caption that you entered is lower than previously captioned"
+              );
+            }
+          }
+        });
+        await bot.sendMessage(chatId, "Caption is correct it is a number");
+      } else {
+        await bot.sendMessage(
+          chatId,
+          "You have entered the incorrect caption it must only be numbers"
+        );
+      }
+    } else {
+      await bot.sendMessage(chatId, "There is no caption on the Photo");
+    }
+
+    // await postToDB(Photo, { firstName, lastName, userName, image });
   });
